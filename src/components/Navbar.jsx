43cd05ac070ad70f188fc219f622f5be10e9b545
Roldan Feliciano <roldan.feliciano12@gmail.com>
2023-08-06 22:55:32 +0800

    styling: register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,29 @@
+import "../style/navbar.scss"
+
 function Navbar() {
   return(
     <>
-     <nav className="navbar bg-body-tertiary fixed-top">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <img src="public/savingsense_logo.png" alt="Savingsense Logo" width="250" />
-        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <img src="./public/savingsense_logo.png" alt=""  width="250" className="savingsence-logo"/>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-          <div className="offcanvas-header">
-          <img src="public/savingsense_logo.png" alt="Savingsense Logo" width="250" />
-            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Date</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Note</a>
-              </li>   
-            </ul>
-            <form className="d-flex mt-3" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className=" navbar-nav d-flex justify-content-center me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
         </div>
       </div>
-     </nav>
+    </nav>
     </>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,32 +1,34 @@
+import { Link } from "react-router-dom"
 import "../style/register.scss"
 
 
 function Register() {
   return(
     <>
-     <div className="signup register d-flex justify-content-center align-items-center vh-100 bg-white">
-      <div className="form_container rounder">
-        <form className="conteiner bg-register p-5">
-          <h3 className="text-center mb-5">Create an account</h3>
+     <div className="signup register d-flex justify-content-center align-items-center vh-100 bg-signup">
+      <div className="form_container">
+        <form className="conteiner bg-register p-5 rounded">
+          <h3 className="text-center mb-3 text-lowercase"><span className="text-uppercase">C</span>reate an account</h3>
           <div className="mb-2">
-            <label htmlFor="fname">Full Name</label>
-            <input type="text" placeholder="Full Name" className="form-control" />
-          </div>
-          <div className="mb-2">
-            <label htmlFor="email">Email</label>
-            <input type="email" placeholder="email" className="form-control" />
+            <label htmlFor="fname"></label>
+            <input type="text" placeholder="Full Name" className="form-control rounded-pill border-input shadow-sm p-2 text-center" required/>
           </div>
           <div className="mb-2">
-            <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Password" className="form-control" />
+            <label htmlFor="email"></label>
+            <input type="email" placeholder="email" className="form-control rounded-pill shadow-sm p-2 text-center" required/>
           </div>
           <div className="mb-2">
-            <label htmlFor="rpassword">Repeat Password</label>
-            <input type="password" placeholder="Repeat Password" className="form-control" />
+            <label htmlFor="password"></label>
+            <input type="password" placeholder="Password" className="form-control rounded-pill shadow-sm p-2 text-center" required/>
           </div>
-          <div className="d-grid justify-content-center btn-register">
-            <button className="btn w-100">Register</button>
+          <div className="mb-2">
+            <label htmlFor="rpassword"></label>
+            <input type="password" placeholder="Repeat Password" className="form-control rounded-pill shadow-sm p-2 text-center" required/>
           </div>
+          <div className="d-flex justify-content-center mt-3">
+          <button className="rounded-pill btn-primary">Register</button>
+          </div>
+          <p className="mt-3 fw-semi bold">Already have an account? <Link to="/">Sign-in</Link></p>
         </form>
       </div>
 
