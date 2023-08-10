b5baa78ae1b7a0febf512f88be3323fc584ded68
Roldan Feliciano <roldan.feliciano12@gmail.com>
2023-08-11 03:46:49 +0800

    styling: dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,8 @@ import "../style/dashboard.scss"
 import Navbar from "./Navbar.jsx";
 import { Link } from "react-router-dom";
 import savingsense1 from "/savingsense1.png";
+import gacempow from "/gacempow.jpg";
+
 
 
 
@@ -24,14 +26,45 @@ function Dashboard() {
       </div>
       <div className="main-container">
         <div className="child1">
-          <ul className="row ul-margin">
-            <Link className="text-dec"><h2 className="side-bar">Date</h2></Link>
-            <Link className="text-dec"><h2 className="side-bar">Note</h2></Link>
-            <Link className="text-dec"><h2 className="side-bar">Expense Category</h2></Link>
+          <div className="profile-pict">
+           <img src={gacempow} alt="Empow car" width="90" className="text-center rounded"/>
+          </div>
+          <h6 className="text-center Profile-name">Profile-name</h6>
+          <ul className="row ul-margin mt-5">
+            <Link className="text-dec mt-3"><h2 className="side-bar">Income Category</h2></Link>
+            <Link className="text-dec mt-3"><h2 className="side-bar">Expense Category</h2></Link>
+            <Link className="text-dec mt-3"><h2 className="side-bar">Date</h2></Link>
+            <Link className="text-dec mt-3"><h2 className="side-bar">Note</h2></Link>
           </ul>
         </div>
         <div className="child2">
-          <h2>body</h2>
+          <h2 className="text-center mt-3 p-2">Dashboard</h2>
+          <div class="row col-12 d-flex justify-content-center">
+            <div class="col-sm-3 mt-3 mb-sm-0 ms-3">
+              <div class="card">
+                <div class="card-body  income">
+                  <h5 class="card-title text-center">Income</h5>
+                  <h5 className="text-center">$0</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3 mt-3 ms-3">
+              <div class="card">
+                <div class="card-body expense">
+                  <h5 class="card-title text-center ">Expense</h5>
+                  <h5 className="text-center">$0</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3 mt-3 ms-3">
+              <div class="card">
+                <div class="card-body balance">
+                  <h5 class="card-title text-center">Balance</h5>
+                  <h5 className="text-center">$0</h5>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
