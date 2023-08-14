59be18cac90bab1a17a8739adde77d3f6e1235db
Christine Elaine Lynch <lalainelynch@gmail.com>
2023-08-14 14:21:03 +0800

    deleted transaction components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
-import Transaction from "./pages/Transaction";
 import Dashboard from "./components/Dashboard";
-
 
 function App() {
   return (
@@ -16,7 +14,6 @@ function App() {
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/transaction" element={<Transaction />} />
         </Routes>
       </BrowserRouter>
       <Footer />
