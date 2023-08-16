b4f43384574d2ab2a1086b1b626fddd982b83ce6
Christine Elaine Lynch <lalainelynch@gmail.com>
2023-08-16 22:50:34 +0800

    added formik on register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,12 +3,36 @@ import "../style/register.scss";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import savingsense from "/savingsense.png";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function Register() {
-  
+  const formik = useFormik({
+    initialValues: {
+      fname: "",
+      email: "",
+      password: "",
+      rpassword: "",
+    },
+
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email address is required"),
+      password: Yup.string().required("Password is required"),
+      rpassword: Yup.string().oneOf(
+        [Yup.ref("password"), ""],
+        "Password must match"
+      ),
+    }),
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   useEffect(() => {
-    document.title = 'Register';
+    document.title = "Register";
   }, []);
 
   const handleSubmit = (event) => {
@@ -24,57 +48,124 @@ function Register() {
     <>
       <div className="signup register d-flex justify-content-center align-items-center vh-100 bg-signup">
         <div className="form_container">
-          <form className="container bg-register p-5 rounded">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="container bg-register p-5 rounded"
+          >
             <div className="logo-center">
-              <img src={savingsense} alt="Savingsense logo" width="300" className="savingsense-logo mb-5" />
+              <img
+                src={savingsense}
+                alt="Savingsense logo"
+                width="300"
+                className="savingsense-logo mb-5"
+              />
             </div>
-            <h3 className="text-center mb-3 text-lowercase"><span className="text-uppercase">C</span>reate an <span className="text-uppercase">A</span>ccount</h3>
+            <h3 className="text-center mb-3 text-lowercase">
+              <span className="text-uppercase">C</span>reate an{" "}
+              <span className="text-uppercase">A</span>ccount
+            </h3>
             <div className="mb-2">
               <label htmlFor="fname"></label>
-              <input type="text" id="fname" placeholder="Full Name" className="form-control rounded-pill border-input shadow-sm p-2 text-center" required />
+              <input
+                type="text"
+                id="fname"
+                placeholder="Full Name"
+                className="form-control rounded-pill border-input shadow-sm p-2 text-center"
+                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.fname}
+              />
             </div>
             <div className="mb-2">
               <label htmlFor="email"></label>
-              <input type="email" id="email" placeholder=" Email" className="form-control rounded-pill shadow-sm p-2 text-center" required />
+              <input
+                type="email"
+                id="email"
+                placeholder=" Email"
+                className="form-control rounded-pill shadow-sm p-2 text-center"
+                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
             </div>
             <div className="mb-2">
               <label htmlFor="password"></label>
-              <input type="password" id="password" placeholder="Password" className="form-control rounded-pill shadow-sm p-2 text-center" required />
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className="form-control rounded-pill shadow-sm p-2 text-center"
+                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
             </div>
             <div className="mb-2">
               <label htmlFor="rpassword"></label>
-              <input type="password" id="rpassword" placeholder="Confirm Password" className="form-control rounded-pill shadow-sm p-2 text-center" required />
+              <input
+                type="password"
+                id="rpassword"
+                placeholder="Confirm Password"
+                className="form-control rounded-pill shadow-sm p-2 text-center"
+                required
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.rpassword}
+              />
             </div>
             <div className="d-flex justify-content-center mt-3">
-              <Link to="/">
-              <button type="submit" className="rounded-pill bg-gerwin" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              {/* <Link to="/"> */}
+              <button
+                type="submit"
+                className="rounded-pill bg-gerwin"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
                 Register
               </button>
-              </Link>
-              <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              {/* </Link> */}
+              <div
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
                 <div className="modal-dialog">
                   <div className="modal-content">
                     <div className="modal-header">
                       <h1 className="modal-title fs-5" id="exampleModalLabel">
                         Thank you!
                       </h1>
-                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
                     </div>
-                    <div className="modal-body">Account has been succussfuly created.</div>
+                    <div className="modal-body">
+                      Account has been succussfuly created.
+                    </div>
                     <div className="modal-footer">
-                      <Link to="/">
-                        <div className="bg-gerwin-modal rounded-pill text-center">
-                          <button type="submit" className="btn rounded-pill">
-                            <span className="modal-save">Save</span>
-                          </button>
-                        </div>
-                      </Link>
+                      {/* <Link to="/"> */}
+                      <div className="bg-gerwin-modal rounded-pill text-center">
+                        <button type="submit" className="btn rounded-pill">
+                          <span className="modal-save">Save</span>
+                        </button>
+                      </div>
+                      {/* </Link> */}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <p className="mt-3 text-center">Already have an account? <Link to="/">Sign-in</Link></p>
+            <p className="mt-3 text-center">
+              Already have an account? <Link to="/">Sign-in</Link>
+            </p>
           </form>
         </div>
       </div>
